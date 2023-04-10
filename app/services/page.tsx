d5902a4ab4817fcf8d2320d3a6a249bb53e4bfcb
Omar Mahmoud <99248837+OmarMahmoud97@users.noreply.github.com>
2023-04-10 14:05:48 +0100

    error fix when transfering data

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 import solo from "../../public/assets/serviceAssets/solo.png";
 import group from "../../public/assets/serviceAssets/group.png";
 import bird from "../../public/assets/serviceAssets/bird.png";
-import bunny from "../../public/assets/serviceAssets/bunny .png";
+import bunn from "../../public/assets/serviceAssets/bunny.png";
 
 Image;
 function Service() {
   return (
-    <section className="h-[120vh] mt-[6rem]  flex items-center justify-center flex-col">
+    <section className="h-[120vh] mt-[6rem] flex items-center justify-center flex-col">
       <h1 className="font text-3xl mb-[2rem] h-fit">Services</h1>
       <div className="flex items-center w-[64rem] justify-evenly h-4/5  flex-col ">
         <div className="flex w-full items-center justify-evenly h-2/5">
@@ -40,7 +40,7 @@ function Service() {
           <article className="flex items-center justify-between flex-col  w-[13rem] service h-full p-8">
             <div className="flex items-center justify-between w-full h-full hover:scale-125 flex-col serv-hover">
               <Image
-                src={bunny}
+                src={bunn}
                 alt="house sitting"
                 className="w-full scale-[1]"
               ></Image>
